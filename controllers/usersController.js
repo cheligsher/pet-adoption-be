4024e4180b52bd03724b3e88ba74a8483f2a3c00
cheligsher <cheligsher@gmail.com>
2023-01-08 16:18:42 +0200

    updated user and pet routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,15 +3,19 @@ const { signUpModel } = require("../models/usersModels")
 const signUp = async (req, res) => {
     try{
         const { email, password, firstName } = req.body;
-    const newUser = {
-        email,
-        password,
-        // password has now been hashed
-        firstName
-    }
+        const newUser = {
+            email,
+            password,
+            // password has now been hashed
+            firstName
+        }
+    //addUser(req.body)
+    // add user with model to add to db
     const userId = await signUpModel(newUser)
     res.send({ userId, email, firstName })
     } catch (err) {
         console.log(err)
     }
 }
+
+module.exports = {signUp}
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,52 +1,52 @@
-const express = require("express")
-const router = express.Router()
-const { passwordsMatch, isNewUser, hashPassword } = require("../middleware/usersMiddleware")
-const {validateBody} = require("../middleware/validateBody");
-const {loginSchema, signUpSchema} = require("../schemas/usersSchema")
+const express = require("express");
+const router = express.Router();
+const {
+  passwordsMatch,
+  isNewUser,
+  hashPassword,
+} = require("../middleware/usersMiddleware");
+const { validateBody } = require("../middleware/validateBody");
+const { loginSchema, signUpSchema } = require("../schemas/usersSchema");
+const usersController = require("../controllers/usersController")
 
-
-
-router.get('/', async (req, res) => {
-    res.send("GET all users (admin only)")
-    // admin only route!
-    //fetc from db
-})
-
-router.post(
-    "/signup",
-    validateBody(signUpSchema),
-    passwordsMatch,
-    isNewUser,
-    hashPassword,
-    // usersController.signup
-    (req, res) => {
-        // addUser(req.body)
-  
-      // add user with model to add to db
-      res.send("signup post successful");
-    }
-);
-
-router.post("/login", 
-validateBody(loginSchema), 
-// doesUserExist, 
-// verify password 
-(req, res) => {
-  //model
-  res.send("login post successful");
+router.get("/", async (req, res) => {
+  res.send("GET all users (admin only)");
+  // admin only route!
+  //fetch from db
 });
 
-router.get('/:id', async (req, res) => {
-    res.send("GET user by id")
-})
+router.post(
+  "/signup",
+  validateBody(signUpSchema),
+  passwordsMatch,
+  isNewUser,
+  hashPassword,
+  usersController.signUp
+);
 
-router.put('/:id', async (req, res) => {
-    res.send("Update user (logged in user only)")
+router.post(
+  "/login",
+  validateBody(loginSchema),
+  // doesUserExist,
+  // verify password
+  (req, res) => {
+    //model
+    res.send("login post successful");
+  }
+);
+
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    res.send("GET user by id");
+  })
+  .put(async (req, res) => {
+    res.send("Update user (logged in user only)");
     //logged in user only
-})
+});
 
-router.get('/:id/full', async (req, res) => {
-    res.send("GET user by id")
-})
+router.get("/:id/full", async (req, res) => {
+  res.send("GET user by id");
+});
 
 module.exports = router;
