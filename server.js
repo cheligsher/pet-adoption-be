d1c91e2da047a82f4a0b062d12d3db4cfbc701d7
cheligsher <cheligsher@gmail.com>
2023-01-12 11:06:42 +0200

    updated DB name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const uri = process.env.MONGO_URI;
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 mongoose.set("strictQuery", false)
-.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, dbName: "pet-adoptionDB"})
 .then((connection) => {
   if(connection) {
     console.log('Connected to DB')
